34caca09e0982a94422c0765b271c79055349420
julietamores <julieta.mores@itscordoba.edu.ar>
2022-04-19 20:30:59 -0300

    version inicial

---
+++ nombre-solucion/nombre-proyecto-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nombre_proyecto_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
